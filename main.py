9175d1496d69765935db1794944297419f717eb9
Huang Peiwen <peiwen.huang@bric.jp>
2023-04-12 19:33:27 +0900

    call webhook function

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import uvicorn
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from routers import webhooks
+
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+app.include_router(webhooks.router)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+@app.get("/liff", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
