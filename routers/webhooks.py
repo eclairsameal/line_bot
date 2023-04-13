c13d50ea8b587754e5ca43501002860d1d5806c6
Huang Peiwen <peiwen.huang@bric.jp>
2023-04-13 09:01:42 +0900

    追加chatgpt功能

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from typing import List, Optional
 from pydantic import BaseModel
 from fastapi import Request, HTTPException, APIRouter, Header
+import openai
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -25,9 +26,33 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+openai.api_key = os.getenv("OPENAPI_KEY")
+conversation = []
+
+class ChatGPT:
+    def __int__(self):
+        pass
+        self.messages = conversation
+        self.model = "gpt-3.5-turbo"
+
+    def get_response(self, user_input):
+        conversation.append({"role": "user", "content": user_input})
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=conversation
+        )
+        conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
+        print("AI回答內容：")
+        print(response['choices'][0]['message']['content'].strip())
+        return response['choices'][0]['message']['content'].strip()
+
+
 class Line(BaseModel):
     destination: str
     events: List[Optional[None]]
+
+
+chatgpt = ChatGPT()
 
 @router.post("/line")
 # 驗證跟接收 LINE 的資訊(Verify and receive LINE information)
@@ -45,7 +70,10 @@ def message_text(event):
     print("!!!!!!!!!!!!!!!!!!!!!!")
     print(event)
     print("!!!!!!!!!!!!!!!!!!!!!!")
+    reply_msg = chatgpt.get_response(event.message.text)
+    print(reply_msg)
+    print("!!!!!!!!!!!!!!!!!!!!!!")
     line_bot_api.reply_message(  # 只能用在接收到其他 LINE 使用者的時候回覆信息，而不能用在主動推送信息
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+        TextSendMessage(text=reply_msg)
     )
